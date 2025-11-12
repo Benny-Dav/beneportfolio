89edbd7d53bf221acdeac33292ebed519bd182b7
Benny-Dav <benedictadavour777@gmail.com>
2025-11-12 16:06:46 +0000

    Add new project image for able.png

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,7 +3,8 @@ import proj2 from "../assets/images/BarkBox.png";
 import proj1 from "../assets/images/oceanViewHotel.png";
 import proj3 from "../assets/images/payne.png";
 import proj4 from "../assets/images/fc.png";
-
+import proj5 from "../assets/images/able.png;
+    
 const projects = [
     {
         image: proj1,
