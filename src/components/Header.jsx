87357ffa6287257ded3cbf89869f25eebd42dae7
Benny-Dav <benedictadavour777@gmail.com>
2026-03-01 20:00:17 +0000

    Update resume link and download attributes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,8 +33,8 @@ const Header = () => {
                 {/* header buttons */}
             <div id="header-btns" className="flex flex-col lg:flex-row gap-4 mt-6 lg:mt-8 w-[90%] justify-center rounded-full py-2 mx-auto text-lg md:mb-[17%]">
                     <a  target="_blank"
-                        href="/Benedicta Davour Professional CV.pdf"
-                        download="Benedicta Davour Professional Resume.pdf"
+                        href="/Benedicta_Davour_Professional_Resume.pdf"
+                        download="Benedicta_Davour_CV.pdf"
                         rel="noopener noreferrer"
                         className="w-full"
                     >
