adb400130fc9c4a6036167571d7fddc62222494c
Benny <benedictadavour777@gmail.com>
2026-06-13 11:12:23 +0000

    Hid firebase secrets

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC_R9-Nsc3JoT3M4ZMIaMxwZcAjfYFhYDA",
-  authDomain: "beneportfolio-79ed3.firebaseapp.com",
-  projectId: "beneportfolio-79ed3",
-  storageBucket: "beneportfolio-79ed3.firebasestorage.app",
-  messagingSenderId: "257279804136",
-  appId: "1:257279804136:web:1f50cd5d363ce9b595bf5d"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// Initialize FireStore
+
 export const db = getFirestore(app);
